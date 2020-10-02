f6fb6d9749ffce6c49cf6ddc10d7036828d37467
jules.jamet <jules.jamet@gmail.com>
2020-10-02 13:51:15 +0100

    Added client project card

--- src/components/ClientsProjects.js
+++ src/components/ClientsProjects.js
@@ -1,9 +1,60 @@
 import React from 'react';
 
+import Zamzam from '../assets/images/zamzam.png';
+
+import Shopify from '../assets/images/shopify.png';
+import Liquid from '../assets/images/liquid.png';
+import Html from '../assets/images/html.png';
+import Css from '../assets/images/css.png';
+import Js from '../assets/images/js.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 class ClientsProjects extends React.Component {
     render() {
         return (
-            <p>testing clinet projects</p>
+            <section>
+                <h2 style={{ fontFamily: 'Source sans Pro, sans-serif', padding: "20px", textAlign: 'left' }} > Clients Projects</h2>
+                <div class="row">
+                    <div class="col-12 col-md-6">   
+                        <div style={{ background: `url(${Zamzam}) center / cover no-repeat` , height: "300px" , margin: '15px' }} /> 
+                        
+                        <div class="shadow" style={{ margin: "15px" , padding: "5px" }}>
+                            <div class="d-flex" style={{ paddingTop: "5px" , paddingBottom: "5px" ,paddingLeft: "20px" }}>
+                                <FontAwesomeIcon icon="star" style={{ fontSize: "16px" }} />
+                                <h6 style={{ paddingLeft: "5px" }}>zamzamjewels.com</h6>
+                            </div>
+                            <p style={{ paddingLeft: "20px" , paddingRight: "10px" }} >Creation of shopify store with design changes. New product page.&nbsp;</p>
+                        <div/>
+
+                        <div class="d-flex" style={{ padding: "10px" , paddingLeft: "20px" }}>
+                            <div style={{ marginRight: "10px" }}>
+                                <img alt="Shopify logo" src={Shopify} width="50"/>
+                                <p class="text-center" style={{ fontSize: "12px" , color: '#868e96' ,marginTop: '10px' }}>Shopify</p>
+                            </div>
+                        
+                            <div style={{ marginRight: '10px' }}>
+                                <img alt="Liquid logo" src={Liquid} width="50"/>
+                                <p class="text-center" style={{ fontSize: "12px" , color: '#868e96' , marginTop: '10px' }}>Liquid</p>
+                            </div>
+
+                            <div style={{ marginRight: '10px' }}>
+                                <img alt="html logo" src={Html} width="53"/>
+                                <p class="text-center" style={{ fontSize: "12px" , color: '#868e96' , marginTop: '10px' }}>html</p>
+                            </div>
+
+                            <div style={{ marginRight: '10px' }}>
+                                <img alt="css logo" src={Css} width="50" style={{ paddingTop: '5px'}}/>
+                                <p class="text-center" style={{ fontSize: "12px" , color: '#868e96' , marginTop: '10px' }}>css</p>
+                            </div>
+                            <div style={{ marginRight: '10px' }}>
+                                <img alt="js logo" src={Js} style={{ paddingTop: '5px' , height: "50px"}}/>
+                                <p class="text-center" style={{ fontSize: "12px" , color: '#868e96' , marginTop: '10px' }}>js</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </section>
         )
     }
 }
--- src/components/fontAwesomeIcons.js
+++ src/components/fontAwesomeIcons.js
@@ -1,5 +1,5 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
-library.add(fab)
+library.add(fab, faStar)
