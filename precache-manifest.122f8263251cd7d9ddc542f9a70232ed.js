875eb337a2deae8e21daaa9118fd87a07f5fe81f
jules.jamet <jules.jamet@gmail.com>
2020-10-12 13:10:24 +0100

    Updates

---
+++ precache-manifest.122f8263251cd7d9ddc542f9a70232ed.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18202b6652bda121329d1ff8e0fcb2be",
+    "url": "/my_portfolio/index.html"
+  },
+  {
+    "revision": "eec7b562e03cd8977f25",
+    "url": "/my_portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "435d8cfff64f549c5fbd",
+    "url": "/my_portfolio/static/css/main.13adb958.chunk.css"
+  },
+  {
+    "revision": "eec7b562e03cd8977f25",
+    "url": "/my_portfolio/static/js/2.aa6fb0a1.chunk.js"
+  },
+  {
+    "revision": "bfd04eb851fbc8ea43c375cc9d698376",
+    "url": "/my_portfolio/static/js/2.aa6fb0a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "435d8cfff64f549c5fbd",
+    "url": "/my_portfolio/static/js/main.086cce72.chunk.js"
+  },
+  {
+    "revision": "d9629bd1ee9955b69a66",
+    "url": "/my_portfolio/static/js/runtime-main.88b1f98b.js"
+  },
+  {
+    "revision": "d5ca19bea4578310d1c27e47d534e625",
+    "url": "/my_portfolio/static/media/bootstrap.d5ca19be.png"
+  },
+  {
+    "revision": "203fd796ab5632c4fb81c2e9a9fdad80",
+    "url": "/my_portfolio/static/media/developer_activity.203fd796.svg"
+  },
+  {
+    "revision": "2e6a13274d98af1bf21da04f44bb42d0",
+    "url": "/my_portfolio/static/media/feelivelove.2e6a1327.png"
+  },
+  {
+    "revision": "f4c1759df56cc94030841c386f10186c",
+    "url": "/my_portfolio/static/media/graphql.f4c1759d.png"
+  },
+  {
+    "revision": "144cfa565509f0ead2c91f7c213c6798",
+    "url": "/my_portfolio/static/media/heroku.144cfa56.png"
+  },
+  {
+    "revision": "b929047a0a254bf9223c65dad85fd4f7",
+    "url": "/my_portfolio/static/media/judicael.b929047a.png"
+  },
+  {
+    "revision": "231af56b715a2f86c2d88748fe561ff5",
+    "url": "/my_portfolio/static/media/liquid.231af56b.png"
+  },
+  {
+    "revision": "ab0fdea9c88bf0224792148866f3ed15",
+    "url": "/my_portfolio/static/media/madariss.ab0fdea9.png"
+  },
+  {
+    "revision": "be74c3c2bc00710463189efc61d18faf",
+    "url": "/my_portfolio/static/media/profile_pic.be74c3c2.jpeg"
+  },
+  {
+    "revision": "a766df31619cc7ab41ab2b3bd421d6de",
+    "url": "/my_portfolio/static/media/rails.a766df31.png"
+  },
+  {
+    "revision": "2a29cdd7a67f37607200f4d52ff19413",
+    "url": "/my_portfolio/static/media/react.2a29cdd7.png"
+  },
+  {
+    "revision": "0c333e6bb7606548c214ecee39736609",
+    "url": "/my_portfolio/static/media/shopify.0c333e6b.png"
+  },
+  {
+    "revision": "94d6a58bf2c902163c6aeef0a587fa9c",
+    "url": "/my_portfolio/static/media/sql.94d6a58b.png"
+  },
+  {
+    "revision": "712e7791aaf25f3f1a2f5d3d747084ee",
+    "url": "/my_portfolio/static/media/thumbnail-MacBook-Air_2x.712e7791.png"
+  },
+  {
+    "revision": "f9eababab7ab7045b14ee94e140a05be",
+    "url": "/my_portfolio/static/media/woudz.f9eababa.png"
+  },
+  {
+    "revision": "803e0f5fbcbd1a4409688d16927a55a1",
+    "url": "/my_portfolio/static/media/zamzam.803e0f5f.png"
+  }
+]);
