7e1d1ad3b12e2c139fc974710d579142d7f66c7f
jules.jamet <jules.jamet@gmail.com>
2020-10-12 13:04:29 +0100

    added toggle button javascript bootstrap/ added gh pages package

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './header.css';
-import Nav from './header_nav';
+import Navigation from './header_nav';
 import Hero from './container_hero';
 
 class Header extends React.Component {
     render() {
         return (
             <div className="header-blue">
-                <Nav navItems={this.props.navItems} />
+                <Navigation navItems={this.props.navItems} />
                 <Hero/>
             </div>
         )   
--- src/components/header_nav.js
+++ src/components/header_nav.js
@@ -2,18 +2,27 @@ import React from 'react';
 import Logo from '../assets/images/Background_logo_small_2.png';
 
 class Nav extends React.Component {
+    state = {
+        navCollapsed: true
+      }
+    
+    _onToggleNav = () => {
+        this.setState({ navCollapsed: !this.state.navCollapsed })
+    }
+    
     render() {
+        const {navCollapsed} = this.state
         return (
             <div className="navbar navbar-light navbar-expand-md navigation-clean-search">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#" >
                         <img alt="Working on computer" src={Logo}/>
                     </a>
-                    <a data-toggle="collapse" className="navbar-toggler" data-target="#navcol-1">
+                    <button aria-expanded='false' className='navbar-toggle collapsed navbar-toggler' onClick={this._onToggleNav}>
                         <span className="sr-only">Toggle navigation</span>
                         <span className="navbar-toggler-icon"/>
-                    </a>
-                    <div className="collapse navbar-collapse" id="navcol-1">
+                    </button>
+                    <div className={(navCollapsed ? 'collapse' : '') + ' navbar-collapse'} id="navcol-1">
                         
                         <ul className="nav navbar-nav ml-auto">
                             { this.props.navItems.map( nav => {
